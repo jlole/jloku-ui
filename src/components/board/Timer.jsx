2341e91b9a1baa456a00e3546794dbcd67e49512
Jean Louis <jeanlouis@grutto.com>
2022-11-06 03:21:06 +0100

    timer

---
+++ src/components/board/Timer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+function Timer(props) {
+
+  const [seconds, setSeconds] = useState(0)
+  const [timerRunning, setTimerRunning] = useState(true)
+
+  // const toggle = () => {
+  //   setTimerRunning(!timerRunning);
+  // }
+
+  // const startTimer = () => {
+  //   setTimerRunning(true);
+  // }
+
+  // const stopTimer = () => {
+  //   setTimerRunning(false);
+  // }
+
+  useEffect(() => {
+    let interval = null;
+    if (timerRunning) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!timerRunning && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timerRunning, seconds]);
+
+  const visualTime = ( seconds ) => {
+    let visualMinutes = Math.floor(seconds / 60)
+    let visualSeconds = seconds - visualMinutes * 60
+    return visualMinutes + ':' + visualSeconds.toString().padStart(2, '0');
+  }
+
+  return (
+    <div className='game-timer'>
+      {visualTime(seconds)}
+    </div>
+  );
+}
+
+export default Timer;
