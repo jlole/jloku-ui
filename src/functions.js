0226cdafcbe6a55ffa67588721323980f1af529b
Jean Louis <jeanlouis@grutto.com>
2022-11-07 16:51:15 +0100

    Remove unsused functions

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,3 @@
-export function convertTableToArray( tableSelector ) {
-  document.querySelector( tableSelector )
-  return 
-}
-
-export function puzzle_row_is_valid() {}
-
-export function puzzle_col_is_valid() {}
-
 export function getApiGameUrl( difficulty ) {
   if ( difficulty === 'daily' ) {
     return '/api/get_daily_board'
