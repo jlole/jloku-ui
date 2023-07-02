d59f1352e294f52a692c30f051b2e13f4df58562
Jean Louis <lehlouisle@gmail.com>
2023-07-02 16:04:02 +0200

    navigation highlight

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const location = useLocation();
+
   return (
     <nav>
       <ul>
-        <li>
+        <li className={location.pathname === '/' ? 'active' : ''}>
           <Link to='/'>Daily</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/easy' ? 'active' : ''}>
           <Link to='/easy'>Easy</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/medium' ? 'active' : ''}>
           <Link to='/medium'>Medium</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/hard' ? 'active' : ''}>
           <Link to='/hard'>Hard</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/extreme' ? 'active' : ''}>
           <Link to='/extreme'>Extreme</Link>
         </li>
       </ul>
